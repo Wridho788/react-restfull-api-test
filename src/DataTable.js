0469f4a8a189650705f5790d0e66f4823ec803bc
Wridho788 <wridho246@gmail.com>
2022-12-24 14:32:43 +0700

    add search

--- src/DataTable.js
+++ src/DataTable.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState, useMemo } from 'react';
 import axios from 'axios';
+// import MaterialReactTable from 'material-react-table';
 import Pagination from './Pagination';
+import TextField from '@mui/material/TextField';
 import Table from '@mui/material/Table';
 import TableBody from '@mui/material/TableBody';
 import TableCell from '@mui/material/TableCell';
@@ -12,6 +14,7 @@ import Paper from '@mui/material/Paper';
 const DataTable = () => {
   const [users, setUsers] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
+  const [search, setSearch] = useState('');
 
   let PageSize = 5;
 
@@ -39,8 +42,18 @@ const DataTable = () => {
 
   return (
     <div>
-      <TableContainer component={Paper}>
-        <Table sx={{ minWidth: 650 }} aria-label='simple table'>
+      <TextField
+        id='outlined-basic'
+        label='Search Name'
+        variant='outlined'
+        sx={{ marginTop: '2rem', width: '80%', backgroud: 'white' }}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+
+      <TableContainer
+        component={Paper}
+        sx={{ width: '80%', margin: '2rem auto' }}>
+        <Table sx={{ minWidth: 650 }} aria-label='simple table' data={users}>
           <TableHead>
             <TableRow>
               <TableCell>ID</TableCell>
@@ -52,20 +65,22 @@ const DataTable = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {currentTableData.map((item, index) => {
-              return (
-                <TableRow
-                  key={index.id}
-                  sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
-                  <TableCell>{item.id}</TableCell>
-                  <TableCell>{item.name}</TableCell>
-                  <TableCell>{item.username}</TableCell>
-                  <TableCell>{item.email}</TableCell>
-                  <TableCell>{item.website}</TableCell>
-                  <TableCell>{item.phone}</TableCell>
-                </TableRow>
-              );
-            })}
+            {currentTableData
+              .filter((user) => user.name.toLowerCase().includes(search))
+              .map((item, index) => {
+                return (
+                  <TableRow
+                    key={index.id}
+                    sx={{ '&:last-child td, &:last-child th': { border: 0 } }}>
+                    <TableCell>{item.id}</TableCell>
+                    <TableCell>{item.name}</TableCell>
+                    <TableCell>{item.username}</TableCell>
+                    <TableCell>{item.email}</TableCell>
+                    <TableCell>{item.website}</TableCell>
+                    <TableCell>{item.phone}</TableCell>
+                  </TableRow>
+                );
+              })}
           </TableBody>
         </Table>
       </TableContainer>
